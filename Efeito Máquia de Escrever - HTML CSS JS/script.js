034db48571e4407a70263c61e121f40145bbaefc
Code em um minuto <120221005+codeemumminuto@users.noreply.github.com>
2023-01-18 20:44:45 -0300

    Efeito Máquia de Escrever - HTML CSS JS

---
+++ Efeito Máquia de Escrever - HTML CSS JS/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function typeWriter(el) {
+    const textArray = el.innerHTML.split('');
+    el.innerHTML = '';
+    textArray.forEach((letter, i) =>
+        setTimeout(() => (el.innerHTML += letter), 55 * i)
+    );
+}
+
+typeWriter(mensagem);
