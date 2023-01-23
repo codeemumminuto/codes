72263708b7ea43a80a2b2cbeac2f903ffc2e8882
Code em um minuto <120221005+codeemumminuto@users.noreply.github.com>
2023-01-23 14:04:38 -0300

    Programa para seguidordo tiktok
    Atualmente eu sou agente educador concursado e trabalho na creche da prefeitura da minha cidade. Como a prefeitura tercerizou algumas creches, foi preciso tirar os agentes educadores dessas escolas e mudar eles para as escolas que não seriam tercerizadas. E pra ficar justo, foi feito uma atribuição onde todos escolhem 5 opções de escolas, e a prioridade de escolha vai ser do melhor classificado no concurso. 
    Eu queria fazer um projeto pra facilitar isso, iria deixar armazenado as escolas disponiveis pra escolha e um determinado numero de vagas pra cada um.
    Ai conforme for escolhendo, vai tirando as vagas da escola.

---
+++ Programa para seguidordo tiktok/cod.py
@@ -0,0 +1,58 @@
+def encontrar_id(nome_escola, dicionario):
+    for id, escola in dicionario.items():
+        if escola['nome'] == nome_escola:
+            return id
+
+escolas = {
+    1:{'nome': 'Jesus de Nazaré', 'vagas': 3},
+    2:{'nome': 'Mario Compagner', 'vagas': 4},
+    3:{'nome': 'Helena Vaz', 'vagas': 2},
+    4:{'nome': 'Cassiano Ricardo', 'vagas': 3},
+    5:{'nome': 'Zenaide Vilalva', 'vagas': 1},
+    6:{'nome': 'Benedito Carvalho', 'vagas': 2},
+}
+
+cadastros = {
+    1:{'nome': 'Ana', 'classificacao': 75, 'opcao1': escolas[1]['nome'], 'opcao2': escolas[5]['nome'], 'opcao3': escolas[3]['nome']},
+    2:{'nome': 'Gabriel', 'classificacao': 140, 'opcao1': escolas[1]['nome'], 'opcao2': escolas[2]['nome'], 'opcao3': escolas[3]['nome']},
+    3:{'nome': 'Sarah', 'classificacao': 55, 'opcao1': escolas[4]['nome'], 'opcao2': escolas[6]['nome'], 'opcao3': escolas[2]['nome']},
+    4:{'nome': 'Giovanna', 'classificacao': 215, 'opcao1': escolas[3]['nome'], 'opcao2': escolas[4]['nome'], 'opcao3': escolas[5]['nome']},
+    5:{'nome': 'Eduardo', 'classificacao': 259, 'opcao1': escolas[5]['nome'], 'opcao2': escolas[3]['nome'], 'opcao3': escolas[1]['nome']},
+    6:{'nome': 'Davi', 'classificacao': 80, 'opcao1': escolas[5]['nome'], 'opcao2': escolas[4]['nome'], 'opcao3': escolas[3]['nome']},
+    7:{'nome': 'Maria', 'classificacao': 201, 'opcao1': escolas[4]['nome'], 'opcao2': escolas[6]['nome'], 'opcao3': escolas[1]['nome']},
+    8:{'nome': 'Nicole', 'classificacao': 100, 'opcao1': escolas[6]['nome'], 'opcao2': escolas[3]['nome'], 'opcao3': escolas[1]['nome']},
+    9:{'nome': 'Natalie', 'classificacao': 189, 'opcao1': escolas[4]['nome'], 'opcao2': escolas[2]['nome'], 'opcao3': escolas[6]['nome']}
+}
+
+#imprime as escolas cadastradas e vagas
+print("Escolas cadastradas: ")
+for id_escola, escola in escolas.items():
+    print(f"{escola['nome']} | Vagas: {escola['vagas']}")
+
+
+#organiza e imprime os cadastros do maior para o menor
+cadastros_ordenado = sorted(cadastros.items(), key=lambda x: x[1]['classificacao'],reverse=True)
+print("\n")
+print("Pessoas cadastradas: ")
+for id_cadastro, cadastro in cadastros_ordenado:
+    print(f"Nome: {cadastro['nome']}")
+    print(f"Classificação: {cadastro['classificacao']}")
+    print(f"Opção 1: {cadastro['opcao1']}")
+    print(f"Opção 2: {cadastro['opcao2']}")
+    print(f"Opção 3: {cadastro['opcao3']}")
+    print()
+
+resultados = list()
+for id_cadastro, cadastro in cadastros_ordenado:
+    for opcao in ['opcao1', 'opcao2', 'opcao3']:
+        nome_escola = cadastro[opcao]
+        id = encontrar_id(nome_escola, escolas)
+
+        if escolas[id]['vagas'] > 0:
+            escolas[id]['vagas'] -= 1
+            resultados.append(f"Concursado: {cadastro['nome']} - Escola: {nome_escola}")
+            break
+
+
+for resultado in resultados:
+    print(resultado)
